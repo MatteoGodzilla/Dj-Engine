d2e615d28e865fbc90f7572cddbd9f812e3f306e
Luca Bassi <luca@argoware.com>
2020-05-14 12:47:24 +0200

    Add Release build type

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ fi
 
 FORCE=false
 DEBUG=false
+RELEASE=false
 
-while getopts "fdh" opt; do
+while getopts "fdrh" opt; do
   case ${opt} in
     f ) echo "FORCE BUILD"
 		FORCE=true
@@ -14,14 +15,19 @@ while getopts "fdh" opt; do
     d ) echo "DEBUG BUILD"
 		DEBUG=true
       ;;
-    h ) echo "Usage: build.sh [-f] [-d]"
+    r ) echo "RELEASE BUILD"
+		RELEASE=true
+      ;;
+    h ) echo "Usage: build.sh [-f] [-d] [-r]"
         echo "-f : Remove cache files and force a clean build"
         echo "-d : Change build type to debug"
+        echo "-r : Change build type to release"
         exit 0
       ;;
-    \? ) echo "Usage: build.sh [-f] [-d]"
+    \? ) echo "Usage: build.sh [-f] [-d] [-r]"
         echo "-f : Remove cache files and force a clean build"
         echo "-d : Change build type to debug"
+        echo "-r : Change build type to release"
         exit 0
       ;;
   esac
@@ -36,6 +42,9 @@ cd build-dir
 if [ $DEBUG = true ]
 then
 	cmake -DCMAKE_BUILD_TYPE=Debug .. && make
+elif [ $RELEASE = true ]
+then
+	cmake -DCMAKE_BUILD_TYPE=Release .. && make
 else
 	cmake .. && make
 fi
